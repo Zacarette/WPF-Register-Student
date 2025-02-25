8c4bdded31a02d42ba3534c2b4090bde997b9749
Michael <85958660+Zacarette@users.noreply.github.com>
2025-02-25 10:17:49 -0500

    Add files via upload

---
+++ CreateClassesObjs (Project Part 2)/CreateClassesObjs/Course.cs
@@ -0,0 +1,23 @@
+﻿namespace CreateClassesObjs
+{
+    public class Course //create public class called Course
+    {
+        private string courseName; //create private string field
+
+        //create public method to set the value of the private field
+        public void setName(string name)
+        {
+            this.courseName = name; //set the value of the private field
+        }
+
+        public string getName()
+        {
+            return this.courseName; //return the value of the private field
+        }
+
+        public override string ToString()
+        {
+            return this.courseName; //control what is displayed in the combo box
+        }
+    }
+}
